277da30ebe107fe7d4c6209ab0fa25ff893a4bab
Emily Watkins <emily.e.dolliver@gmail.com>
2018-04-01 20:57:20 -0700

    add recipe content, show on home page.

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +12,10 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
   }
+
+  recipes: Recipe[] = [
+    new Recipe("Title: Pigs in a blanket", "Description: The only meat + bread you'll ever need", "Body: put a hotdog in some dough and bake it.", "Image url: some great shot.jpg"),
+    new Recipe("Title2", "Description2", "Body2", "Image2")
+  ];
 
 }
